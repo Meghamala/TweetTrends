5766d4fccbe6d5dfdbbbf785249647147e1a2a64
Meghamala <meghamala1996@gmail.com>
2024-03-01 11:17:35 -0800

    Initial Commit

---
+++ src/main/java/com/application/tweettrend/TweetTrendApplication.java
@@ -0,0 +1,13 @@
+package com.application.tweettrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetTrendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/tweettrend/TweetTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.tweettrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetTrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
